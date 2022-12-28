281de6f4863507c877efc50c21dc8517e5479fab
Milena Erbez <erbez01@gmail.com>
2022-12-28 16:12:53 +0100

    rutiranje prvi deo

--- onlinefilm/routes/api.php
+++ onlinefilm/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Resources\GenreResource;
+use App\Http\Controllers\GenreController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/users', [UserController::class, 'index']);
+Route::resource('genres', GenreController::class);
