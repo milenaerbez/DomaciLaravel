2eb6560b83559d17bfdba81d06c4d9349da5f1fe
Milena Erbez <erbez01@gmail.com>
2022-12-28 15:34:35 +0100

    popunjene tabele zanr i glumci pomocu databaseseed

--- onlinefilm/database/seeders/DatabaseSeeder.php
+++ onlinefilm/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Genre;
+use App\Models\Actor;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +17,49 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
+
+
+        //  \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        Actor::truncate();
+        Genre::truncate();
+        User::truncate();
+        User::factory(5)->create();
+        Genre::create([
+            'name' => "Komedija"
+        ]);
+        Genre::create([
+            'name' => "Triler"
+        ]);
+        Genre::create([
+            'name' => "Horor"
+        ]);
+        Genre::create([
+            'name' => "Naucna fantastika"
+        ]);
+        Genre::create([
+            'name' => "Ljubavni"
+        ]);
+
+        Actor::create([
+            'first_name' => "Brad",
+            'last_name' => "Pitt"
+        ]);
+        Actor::create([
+            'first_name' => "Leonardo",
+            'last_name' => "DiCaprio"
+        ]);
+        Actor::create([
+            'first_name' => "Zoe",
+            'last_name' => "Saldana"
+        ]);
+        Actor::create([
+            'first_name' => "Milos",
+            'last_name' => "Bikovic"
+        ]);
     }
 }
